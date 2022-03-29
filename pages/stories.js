7680afd2bb2d961aa7777ffb4ea2ba9e40694a9e
Christopher <dagswebb@gmail.com>
2022-03-29 14:16:19 +0100

    added JSON stories array onto page

--- pages/stories.js
+++ pages/stories.js
@@ -2,15 +2,20 @@ import view from '../utils/view.js';
 
 export default async function Stories(path) {
    const stories = await getStories(path);
+   const hasStories = stories.length > 0;
 
-
-    view.innerHTML = `<div>${path}</div>`;
+    view.innerHTML = `<div>
+    ${hasStories ? stories.map(story => JSON.stringify(story)) : 'No stories'}
+    </div>`;
 }
 
 async function getStories(path) {
     const isHomeRoute = path === '/';
+    const isNewRoute = path === '/new';
     if (isHomeRoute) {
         path = '/news';
+    } else if (isNewRoute) {
+        path = '/newest';
     }
     const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
     const stories = await response.json();
