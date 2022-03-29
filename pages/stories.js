e4837398bfc7689bacc6f144becc3ab8a80713a3
Christopher <dagswebb@gmail.com>
2022-03-29 14:09:14 +0100

    added async functions for fetching

--- pages/stories.js
+++ pages/stories.js
@@ -1,7 +1,20 @@
 import view from '../utils/view.js';
 
-export default function Stories(path) {
+export default async function Stories(path) {
+   const stories = await getStories(path);
+
+
     view.innerHTML = `<div>${path}</div>`;
+}
+
+async function getStories(path) {
+    const isHomeRoute = path === '/';
+    if (isHomeRoute) {
+        path = '/news';
+    }
+    const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
+    const stories = await response.json();
+    return stories;
 }
 
 //https://node-hnapi.herokuapp.com
